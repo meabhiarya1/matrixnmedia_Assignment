60773309d825148650b8b25774c427c1e5159659
Abhishek kumar <abhi.arya97161@gmail.com>
2025-06-09 17:50:48 +0530

    Refactor Register component: remove unused Link import to streamline code

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { registerUser } from "../features/auth/authSlice";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const dispatch = useDispatch();
