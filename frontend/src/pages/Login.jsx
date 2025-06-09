f2c4cc7d2997893d107ae9433a83df4911f8c386
Abhishek kumar <abhi.arya97161@gmail.com>
2025-06-09 17:40:58 +0530

    Refactor Login component: update layout and styling, enhance form structure, and improve user experience with better input handling and navigation.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <form
+      {/* <form
         onSubmit={handleSubmit}
         className="bg-white p-6 rounded shadow-md w-full max-w-md"
       >
@@ -55,7 +55,49 @@ export default function Login() {
             Register
           </Link>
         </p>
-      </form>
+      </form> */}
+
+      <div className="flex flex-col items-center justify-center h-screen dark w-[600px]">
+        <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-6">
+          <h2 className="text-2xl font-bold text-gray-200 mb-4">Login</h2>
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <input
+              type="email"
+              required
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            />
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            />
+            <div className={`flex items-center justify-between flex-wrap`}>
+              <p className="text-white mt-4">
+                Don't have an account?
+                <span
+                  className="text-sm text-blue-500 -200 hover:underline mt-4 cursor-pointer"
+                  onClick={() => navigate("/register")}
+                >
+                  {" "}
+                  Signup
+                </span>
+              </p>
+            </div>
+            <button
+              className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={loading}
+            >
+              {loading ? "Logging in..." : "Login"}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
